90ef83c0058eded9581b2e0c9548c324faa6c0b7
Daniel Alexander Kates <alex.kates@creditly.com>
2021-08-27 16:26:27 -0400

    added typings to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,35 @@
 import typescript from 'rollup-plugin-typescript2';
 import * as packageJson from './package.json';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/Index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    typescript({
-      useTsconfigDeclarationDir: true,
-      clean: true,
-    }),
-  ],
-  external: [],
-};
+const config = [
+  {
+    input: 'src/Index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+        clean: true,
+      }),
+    ],
+    external: [],
+  },
+  {
+    input: './build/Index.d.ts',
+    output: [{ file: 'build/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
+
+export default config;
